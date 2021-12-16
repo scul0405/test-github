4ad0733e4bc1beaac6b0a8dd36f0742e8f907804
Scul <vldtruong1221@gmail.com>
2021-12-16 15:25:32 +0700

    add prototype die and eat for cat

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
 function cat(name, age) {
     this.name = name;
     this.age = age;
+    this.die = false;
+    this.stomatch = [];
+}
+
+cat.prototype.die () {
+    this.die = true;
+}
+
+cat.prototype.eat = function(mouse) {
+    this.stomatch.push(mouse);
+    mouse.die;
 }
 
 module.exports = cat;
